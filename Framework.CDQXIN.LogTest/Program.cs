a1c6db8f8926605f039f3dbdebf72ea231d814be
Administrator <donchen1990@163.com>
2019-11-05 14:18:03 +0800

    添加项目文件。

---
+++ Framework.CDQXIN.LogTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using Framework.CDQXIN.Log;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Framework.CDQXIN.LogTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("开始了");
+            var filepath = AppDomain.CurrentDomain.BaseDirectory + "log4net.config";
+            log4net.Config.XmlConfigurator.Configure(new FileInfo(filepath));
+            LogManager.DefaultLogger.ErrorFormat("hahhahah");
+            LogManager.DefaultLogger.InfoFormat("hahhahah");
+            LogManager.DefaultLogger.Info("1111");
+            Console.WriteLine("结束了");
+            Console.ReadLine();
+        }
+    }
+}
